de626bd3ddb45804172df78e3f6624c99a728fc9
unknown <avdheshg804@gmail.com>
2022-04-22 13:00:59 +0530

    complete project

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,9 +1,10 @@
 import React, { useState } from "react";
 
-import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
 import "./Expenses.css";
 import ExpensesFilter from "./ExpensesFilter";
+import ExpensesList from "./ExpensesList";
+import ExpensesChart from "./ExpensesChart";
 
 const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState("2020");
@@ -12,25 +13,30 @@ const Expenses = (props) => {
     setFilteredYear(selectedYear);
   };
 
-  
+  const filteredExpenses = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  }); 
+
+
 
   return (
-    <Card className="expenses">
-      <ExpensesFilter
-        selected={filteredYear}
-        onChangeFilter={filterChangeHandler}
-      />
-
-      {props.items.map((expense) => (
-        <ExpenseItem
-          key={expense.id}
-          title={expense.title}
-          amount={expense.amount}
-          date={expense.date}
+    <div>
+      <Card className="expenses">
+        <ExpensesFilter
+          selected={filteredYear}
+          onChangeFilter={filterChangeHandler}
         />
-      ))}
 
-    </Card>
+        {/* Chart Component */}
+        <ExpensesChart expenses={filteredExpenses} />
+
+        {/* extracted out so that V can render conditional message if no data found */}
+        <ExpensesList items={filteredExpenses} />
+
+      </Card>
+    </div>
+    
+
   );
 };
 
